6421e912aed0095a239d585bb9c50f4e9ab0f614
crocdemon <abhinavmaurya3@gmail.com>
2022-06-08 23:37:47 +0530

    Add an option to build given solution

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # VisualStudioFileOpenTool
 Visual Studio File Open Tool
 
-	usage: <openf|debug|openp|stopdebug> <visual_studio_path> <solution_path> [<file_path> <file_line_number>] |[<PID_of_process_to_attach_to_debugger>]
+	usage: <openf|debug|openp|stopdebug|build|rebuild> <visual_studio_path> <solution_path> [<file_path> <file_line_number>] |[<PID_of_process_to_attach_to_debugger> | <Build_Config>]
 	
 	The arguments in the bracket are conditional. If first agument is "openf", then filePath and fileLine is required, if it is "debug" PID is required otherwise no fourth argument is needed.
 	
@@ -13,6 +13,10 @@ Visual Studio File Open Tool
 	
 	<stopdebug> is for stopping the debugger and detaching any attached process
 	
+	<build> is for building the solution
+	
+	<rebuild> is for rebuilding the solution
+	
 	<visual_studio_path> is the path to visual studio executable(devenv.exe)
 	
 	<solution_path> is the path to the project solution(.sln) file
@@ -22,3 +26,5 @@ Visual Studio File Open Tool
 	<file_line_number> is the line number that should be in focus after opening the file at <file_path>
 	
 	<PID_of_process_to_attach_to_debugger> is the process ID of the process that needs debugging by attaching it to the Visual Studio Debugger
+	
+	<Build_Config> The config of the build to made(Debug, Release)
--- VisualStudioTool/Program.cs
+++ VisualStudioTool/Program.cs
@@ -22,7 +22,15 @@ namespace VisualStudioTool
             bool attachDebugger = args[0].Equals("debug", StringComparison.OrdinalIgnoreCase);
             bool stopDebugger = args[0].Equals("stopdebug", StringComparison.OrdinalIgnoreCase);
             bool openFile = args[0].Equals("openf", StringComparison.OrdinalIgnoreCase);
-            if (attachDebugger && args.Length < 4)
+            bool buildSolution = args[0].Equals("build", StringComparison.OrdinalIgnoreCase);
+            bool reBuildSolution = args[0].Equals("rebuild", StringComparison.OrdinalIgnoreCase);
+
+            if(buildSolution && args.Length < 4)
+            {
+                Console.WriteLine("Too few arguments to build the project.");
+                return;
+            }
+            else if (attachDebugger && args.Length < 4)
             {
                 Console.WriteLine("Too few arguments to attach to debugger.");
                 return;
@@ -37,6 +45,18 @@ namespace VisualStudioTool
             string solutionPath = args[2];
             try
             {
+                if (buildSolution)
+                {
+                    string buildType = args[3];
+                    BuildSolution(vsPath, solutionPath, buildType);
+                    return;
+                }
+                if(reBuildSolution)
+                {
+                    string buildType = args[3];
+                    RebuildSolution(vsPath, solutionPath, buildType);
+                    return;
+                }
                 DTE dte = FindRunningVSWithOurSolution(solutionPath);
                 if (dte == null)
                 {
@@ -204,7 +224,7 @@ namespace VisualStudioTool
                 return null;
             }
 
-            System.Diagnostics.Process devenv = System.Diagnostics.Process.Start(vsPath, solutionPath);
+            System.Diagnostics.Process devenv = System.Diagnostics.Process.Start(vsPath, QuotePathIfNeeded(solutionPath));
 
 
             DTE dte = null;
@@ -221,6 +241,15 @@ namespace VisualStudioTool
             return dte;
         }
 
+        private static string QuotePathIfNeeded(string path)
+        {
+            if (!path.Contains(" "))
+            {
+                return path;
+            }
+            return "\"" + path + "\"";
+        }
+
         [DllImport("user32.dll")]
         private static extern bool SetForegroundWindow(IntPtr hWnd);
 
@@ -231,7 +260,7 @@ namespace VisualStudioTool
                 return;
             }
             dte.MainWindow.Activate();
-            SetForegroundWindow(new IntPtr(dte.MainWindow.HWnd));
+            SetForegroundWindow(dte.MainWindow.HWnd);
         }
 
         private static void HaveRunningVSOpenFile(DTE dte, string filePath, int fileLine)
@@ -239,7 +268,8 @@ namespace VisualStudioTool
             OpenSolutionInVS(dte);
 
             dte.ItemOperations.OpenFile(filePath);
-            dte.ActiveDocument.Selection.GotoLine(fileLine, true);
+            var textSelection = dte.ActiveDocument.Selection as TextSelection;
+            textSelection.GotoLine(fileLine, true);
         }
 
         private static void AttachDebugger(DTE dte, int processID)
@@ -255,6 +285,47 @@ namespace VisualStudioTool
         private static void StopDebugger(DTE dte)
         {
             dte.Debugger.Stop();
+        }
+
+        private static void  RebuildSolution(string vsPath, string solutionPath, string buildConfig)
+        {
+            var arg = $"{QuotePathIfNeeded(solutionPath)} /Rebuild {buildConfig}";
+            BuildSolutionInternal(vsPath, arg);
+        }
+
+        private static void BuildSolution(string vsPath, string solutionPath, string buildConfig)
+        {
+            var arg = $"{QuotePathIfNeeded(solutionPath)} /Build {buildConfig}";
+            BuildSolutionInternal(vsPath, arg);
+        }
+
+        private static void BuildSolutionInternal(string vsPath, string arguemnts)
+        {
+            var processInfo = new System.Diagnostics.ProcessStartInfo(vsPath, arguemnts);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            var devenv = System.Diagnostics.Process.Start(processInfo);
+            devenv.ErrorDataReceived += (object sender, System.Diagnostics.DataReceivedEventArgs e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    Console.WriteLine($"Debug>> {e.Data}");
+                }
+            };
+            devenv.BeginErrorReadLine();
+
+            devenv.OutputDataReceived += (object sender, System.Diagnostics.DataReceivedEventArgs e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    Console.WriteLine($"Debug>> {e.Data}");
+                }
+            };
+            devenv.BeginOutputReadLine();
+            devenv.WaitForExit();
         }
     }
 }
